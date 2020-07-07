eb94dfc40daf283d32cd809b6aef689591efa284
ktowen <pedroall21@gmail.com>
2020-07-07 16:16:42 -0400

    Cambiados los serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,18 +3,6 @@ from rest_framework import serializers
 from .models import Oficoda, Reparto, Bodega, Distribucion, Producto
 
 
-class OficodaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Oficoda
-        fields = ["id", "nombre", "reparto"]
-
-
-class RepartoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reparto
-        fields = ["id", "nombre"]
-
-
 class BodegaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bodega
@@ -24,21 +12,26 @@ class BodegaSerializer(serializers.ModelSerializer):
             "unidad",
             "n_1_6",
             "n_7_30",
-            "details",
+            "conteo_productos",
             "oficoda",
         ]
 
 
 class DistribucionSerializer(serializers.ModelSerializer):
+    bodegas_detalles = BodegaSerializer(source="bodegas", many=True, read_only=True)
+
     class Meta:
         model = Distribucion
         fields = [
             "id",
             "cantidad",
+            "repartido",
+            "sobrante",
             "fecha",
             "fecha_creacion",
             "producto",
             "bodegas",
+            "bodegas_detalles",
         ]
 
 
@@ -51,3 +44,17 @@ class ProductoSerializer(serializers.ModelSerializer):
             "notas",
         ]
 
+
+class RepartoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reparto
+        fields = ["id", "nombre"]
+
+
+class OficodaSerializer(serializers.ModelSerializer):
+    reparto = RepartoSerializer(read_only=True)
+    bodegas = BodegaSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Oficoda
+        fields = ["id", "nombre", "reparto", "bodegas"]
